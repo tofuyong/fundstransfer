6f6df5877192fab336852799ed16dc56d4b0963d
your_name <andreayong@Andreas-MacBook-Air.local>
2023-03-24 18:14:47 +0800

    first commit

---
+++ src/main/java/com/codeplayground/FundsTransfer/FundsTransferApplication.java
@@ -0,0 +1,13 @@
+package com.codeplayground.FundsTransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundsTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundsTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeplayground/FundsTransfer/FundsTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeplayground.FundsTransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundsTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
